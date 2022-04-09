b53def33cda799a26b1dd43facda5d560d283b2f
B!gRice <81027382+jerry8hero@users.noreply.github.com>
2022-04-09 09:40:34 +0800

    Create qywx.sh

---
+++ qywx.sh
@@ -0,0 +1,9 @@
+curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0837deb9-ddce-4477-8851-a6f40ebcf361' \
+   -H 'Content-Type: application/json' \
+   -d '
+   {
+        "msgtype": "text",
+        "text": {
+            "content": "hello world"
+        }
+   }'
