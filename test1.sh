423d61fdd48aae84b083d54c006aa0403d566f65
jerry8hero <jerry8hero@sina.com>
2022-05-07 09:36:39 +0800

    node test

--- test1.sh
+++
@@ -1,9 +0,0 @@
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0837deb9-ddce-4477-8851-a6f40ebcf361' \
-   -H 'Content-Type: application/json' \
-   -d '
-   {
-        "msgtype": "text",
-        "text": {
-            "content": " 今天是2022年4月20日 "
-        }
-   }'
--- test2.sh
+++
@@ -1,9 +0,0 @@
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0837deb9-ddce-4477-8851-a6f40ebcf361' \
-   -H 'Content-Type: application/json' \
-   -d '
-   {
-        "msgtype": "text",
-        "text": {
-            "content": " vscode github action 插件测试 "
-        } 
-   }'
