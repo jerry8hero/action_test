d49414b2024bcd421e7b4ac6e6351fd43e327e63
jerry8hero <jerry8hero@sina.com>
2022-06-22 09:28:27 +0800

    modify md message

--- qywx.sh
+++ qywx.sh
@@ -1,12 +1,6 @@
 curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=0837deb9-ddce-4477-8851-a6f40ebcf361' \
    -H 'Content-Type: application/json' \
    -d '
-#    {
-#         "msgtype": "text",
-#         "text": {
-#             "content": "大家记得在8点30分前把日计划分享出来哟!",
-#             "mentioned_list":["litinghui"]
-#         }
 {
     "msgtype": "markdown",
     "markdown": {
